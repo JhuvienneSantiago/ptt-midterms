77f04051e8630c8d518216515742934ccbd665a5
SANTIAGO <jtsantiago@fit.edu.ph>
2025-10-04 14:04:48 +0800

    Fixed input variable names

--- toDoApp.py
+++ toDoApp.py
@@ -1,20 +1,20 @@
 # toDoApp.py
 
-tasks=[]
+tasksList=[]
 
 def addtask(task) :
-  tasks.append(task)
+  tasksList.append(task)
   print("task added!")
 
 def showTasks( ):
-    if len(tasks)==0 :
+    if len(tasksList)==0 :
       print("no tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+     for i in range (len(tasksList)):
+      print(i+1,".",tasksList[i])
 
-def removetask(tasknumber):
-    tasks.pop(tasknumber) 
+def removetask(taskNumber):
+    tasksList.pop(taskNumber) 
     print("task removed!!")
 
 def main():
@@ -23,17 +23,17 @@ def main():
         print("2.Show Tasks")
         print("3.Remove Task")
         print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
-            addtask(t)
-        elif ch=="2":
+        choice = input("enter choice : ")
+        if choice=="1":
+            taskToDo = input("enter task : ")
+            addtask(taskToDo)
+        elif choice=="2":
             showTasks()
-        elif ch=="3":
+        elif choice=="3":
             n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+            removetask(n)
+        elif choice=="4":
+            break
         else:
             print("wrong choice!!")
 main()
